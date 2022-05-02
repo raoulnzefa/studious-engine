1e3a56b075c7a697f5ad3a30a427f43a851b2b67
roma_marshall <roma_marshall@outlook.com>
2022-05-02 22:51:12 +0200

    header styling

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,22 @@
           {{ item }}
         </a>
       </nav>
+      <div class="flex items-center">
+        <button class="h-16 px-6">
+          <MenuIcon />
+        </button>
+        <button class="h-16 px-6">
+          <SearchIcon />
+        </button>
+      </div>
     </header>
   </div>
 </template>
 
 <script setup>
 import Logo from './assets/img/logo.svg'
+import MenuIcon from './assets/img/menu.svg'
+import SearchIcon from './assets/img/search.svg'
 
 const menu = ['shop', 'collections', 'new arrival', 'customers']
 </script>

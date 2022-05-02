3f1a04fa7a157cd5e290ef95403affe883e5db7a
roma_marshall <roma_marshall@outlook.com>
2022-05-02 22:46:04 +0200

    menu added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="font-sans text-gray-900">
-    <header>
+    <header class="flex items-center border-b border-gray-900/20">
       <a href="#">
-        <Logo />
+        <Logo/>
       </a>
+      <nav class="flex items-center space-x-10 text-sm font-medium text-gray-900">
+        <a v-for="item in menu" href="#">{{ item }}</a>
+      </nav>
     </header>
   </div>
 </template>
 
 <script setup>
 import Logo from './assets/img/logo.svg'
+
+const menu = ['shop', 'collections', 'new arrival', 'customers']
 </script>
 
 <style scoped>

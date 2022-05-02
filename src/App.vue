78050178145090cdd3b0c98eb63ea9758895d325
roma_marshall <roma_marshall@outlook.com>
2022-05-03 00:13:14 +0200

    menu edit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="font-sans text-gray-900">
     <header class="flex items-center border-b border-gray-900/20">
-      <a href="#">
-        <Logo/>
-      </a>
-      <nav class="flex items-center space-x-10 text-sm font-medium text-gray-900 uppercase">
-        <a
-            href="#"
-            v-for="item in menu"
-            class="py-1 hover:border-b hover:border-gray-900"
-        >
-          {{ item }}
+      <div class="flex items-center justify-between flex-1 pl-20 pr-4">
+        <a href="#">
+          <Logo/>
         </a>
-      </nav>
-      <div class="flex items-center">
-        <button class="h-16 px-6">
-          <MenuIcon />
-        </button>
-        <button class="h-16 px-6">
-          <SearchIcon />
-        </button>
+        <nav class="flex items-center space-x-10 text-sm font-medium text-gray-900 uppercase">
+          <a
+              href="#"
+              v-for="item in menu"
+              class="py-1 hover:border-b hover:border-gray-900"
+          >
+            {{ item }}
+          </a>
+        </nav>
+        <div class="flex items-center">
+          <button class="h-16 px-6">
+            <MenuIcon />
+          </button>
+          <button class="h-16 px-6">
+            <SearchIcon />
+          </button>
+        </div>
       </div>
+      <button class="flex items-center justify-center h-16 px-6 border-l border-gray-900/20">
+        <CartIcon />
+      </button>
     </header>
   </div>
 </template>
@@ -29,6 +34,7 @@
 import Logo from './assets/img/logo.svg'
 import MenuIcon from './assets/img/menu.svg'
 import SearchIcon from './assets/img/search.svg'
+import CartIcon from './assets/img/cart.svg'
 
 const menu = ['shop', 'collections', 'new arrival', 'customers']
 </script>
